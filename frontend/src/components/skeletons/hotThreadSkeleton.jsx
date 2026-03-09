e9f657eb5e6d29aac31b6fc48b4899e19f41843f
Seniru <senirupasan@gmail.com>
2026-03-09 23:48:10 +0530

    Improve layout and responsiveness of forum components

--- frontend/src/components/skeletons/hotThreadSkeleton.jsx
+++ frontend/src/components/skeletons/hotThreadSkeleton.jsx
@@ -4,8 +4,8 @@ import BlockSkeleton from "./blockSkeleton"
 export default function HotThreadSkeleton() {
   return new Array(3).fill(0).map(() => (
     <Container>
-      <BlockSkeleton className="w-80 my-2" />
-      <BlockSkeleton className="w-32 mt-4" />
+      <BlockSkeleton className="w-5/6 my-2" />
+      <BlockSkeleton className="w-1/5 mt-4" />
     </Container>
   ))
 }
